314bcdf980ed11053f8c34222bf79cde959a1921
Rohan <sarith21@gmail.com>
2015-07-19 08:28:35 -0700

    Initial Commit

---
+++ PitchPerfect-submission/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect-submission
+//
+//  Created by Rohan Sarith Pethiyagoda on 19/07/2015.
+//  Copyright (c) 2015 RP3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PitchPerfect-submissionTests/PitchPerfect_submissionTests.swift
@@ -0,0 +1,36 @@
+//
+//  PitchPerfect_submissionTests.swift
+//  PitchPerfect-submissionTests
+//
+//  Created by Rohan Sarith Pethiyagoda on 19/07/2015.
+//  Copyright (c) 2015 RP3. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PitchPerfect_submissionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
